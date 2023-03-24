952ce10f53b794150e21e09e58336ba151ca5315
Sayan100100 <smondal0203100@gmail.com>
2023-03-24 12:38:28 +0530

    frist commit from newbranch

--- test4.java
+++ test4.java
@@ -10,6 +10,8 @@ public class test4 {
 		List<Integer> list=Arrays.asList(1,2,3,4,5,null);
 		Consumer<Integer> t=(data)->{System.out.println(data);};
 		list.forEach(t);
+		System.out.println("too good");
+		System.out.println("very good");
 	}
 
 }
